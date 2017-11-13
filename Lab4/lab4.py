4cb01cc207300570b25b403a1d3839e1ba5e94e5
mor19-meet <mor19@meet.mit.edu>
2017-11-13 18:37:02 +0200

    Uploaded my labs

---
+++ Lab4/lab4.py
@@ -0,0 +1,81 @@
+##class animal(object):
+##	def __init__(self, sound, name, age, favorite_color):
+##		self.sound = sound
+##		self.name = name
+##		self.age = age
+##		self.favorite_color = favorite_color
+##	def eat(self, food):
+##		print("yummy!! " + self.name + "is eating " + food )
+##	def description(self):
+##		print(self.name + "is " + self.age + " years old and loves the color " + self.favorite_color)
+##	def make_sound(self, x):
+##		print(self.sound*x)
+##
+##
+##
+##dog =animal("woof ", "tamer ", "15", "black")
+##dog.eat("shntsel")
+##dog.description()
+##dog.make_sound(3)
+
+
+
+
+
+
+
+
+
+##class person(object):
+##        def __init__(self,name,age,city,gender):
+##                self.name = name
+##                self.age = age
+##                self.city = city
+##                self.gender = gender
+##        def eat(self,Breakfast):
+##                print(self.name + " just ate " + Breakfast)
+##        def hobby(self,hobby):
+##                print(self.name + " enjoys doing " + hobby)
+##
+##
+##
+##
+##tamer = person("tamer", 15, "tmra", "female")
+##tamer.eat("rocks")
+##tamer.hobby("drugs")
+
+
+class song(object):
+        def __init__(self,lyrics):
+                self.lyrics = lyrics
+        def sing_me_a_song(self,lyrics):
+                for index in len(babydonthurtme()):                
+                        print(index[0])
+
+
+
+
+babydonthurtme = ["IF you are happy and you know it clap your hands", "clap clap clap"]
+babydonthurtme.sing_me_a_song
+                
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
