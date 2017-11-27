59c206e89fa44b789f78a5b13f45f38a1bf26719
mor19-meet <mor19@meet.mit.edu>
2017-11-27 19:02:02 +0200

    Lab 5 so many things

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -2,20 +2,43 @@ from turtle import Turtle,colormode
 import turtle
 import random
 
-colormode(255)
-class Square(Turtle):
+##colormode(255)
+##class Square(Turtle):
+##        def __init__(self,size):
+##                Turtle.__init__(self)
+##                self.shapesize(size)
+##                self.shape("square")
+##        def random_color(self):
+##                
+##                r = random.randint(0,256)
+##                b = random.randint(0,256)
+##                g = random.randint(0,256)
+##                self.color(r,b,g)       
+##
+##square = Square(10)
+##square.random_color()
+
+
+class Hexagon(Turtle):
         def __init__(self,size):
                 Turtle.__init__(self)
+                self.begin_poly()
+                for i in range(6):
+                        self.forward(size)
+                        self.left(360/6)
+                self.end_poly()
+                turtle.register_shape("hexagon",self.get_poly())
+                self.shape("hexagon")
                 self.shapesize(size)
-                self.shape("square")
-        def random_color(self):
                 
-                r = random.randint(0,256)
-                b = random.randint(0,256)
-                g = random.randint(0,256)
-                self.color(r,b,g)       
+                
 
-square = Square(10)
-square.random_color()
+
+hexagon = Hexagon(random.randint(0,256))
+
 turtle.mainloop()
+
+
+
+
 
